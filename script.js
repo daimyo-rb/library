c1142df51d29a753ae7825008e1d42da8bb1689b
daimyo-rb <daimyo@roshibros.com>
2025-02-27 15:12:24 -0600

    Refactor to use classes. Book, Library, LibraryScreenRenderer

--- script.js
+++ script.js
@@ -1,146 +1,152 @@
-const cardContainer = document.getElementById('card-container');
-const addBookModal = document.getElementById("addBookModal");
-const addBookBtn = document.getElementsByClassName("add-book-btn")[0];
-const closeModalBtn = document.getElementById("closeModalBtn");
-const addBookForm = document.getElementById("form-add-book");
-let counter = 0;
-
-const myLibrary = [];
-
-function Book(title, author, numPages, haveRead) {
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.haveRead = haveRead;
-    this.id = counter;
-    counter += 1;
+class Book {
+    constructor(title, author, numPages, haveRead, id) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+        this.haveRead = haveRead;
+        this.id = id;
+    }
 }
-function addBookToLibrary(book){
-    myLibrary.push(book);
+
+class Library {
+    constructor () {
+        this.counter = 0;
+        this.bookArray = [];
+    }
+    addBookToLibrary(book){
+        book.id = this.counter;
+        this.counter += 1;
+        this.bookArray.push(book);
+    }
+    removeBookFromLibrary(id){
+        for (let i = 0; i < this.bookArray.length; i++){
+            if (this.bookArray[i].id == id) {
+                this.bookArray.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    toggleHaveRead(id) {
+        for (let i = 0; i < this.bookArray.length; i++){
+            if (this.bookArray[i].id == id) {
+                this.bookArray[i].haveRead = !this.bookArray[i].haveRead;
+            }
+        }
+    }
+
 }
-function createCardElementFromBook(book){
-    let title = book.title; 
-    let author = book.author;
-    let numPages = book.numPages;
-    let haveRead = book.haveRead;
-    let id = book.id
-    let checkedStr = haveRead ? ' checked' : '';
-    const cardHTML = `
-        <div class="card" data-id=${id}>
-            <span class="delete-card-btn" data-id=${id}>&times;</span>
-            <div class="card-inner">
-                <div class="card-upper">
-                    <p class="card-book-title">${title}</p>
-                    <p class="card-book-author">by ${author}</p>
-                    <p class="card-book-pages">${numPages} pages</p>
-                </div>
-                <div class="card-lower">
-                    <p>read?</p>
-                    <input class="card-book-hasRead" type="checkbox"${checkedStr} data-id=${id}>
+
+class LibraryScreenRenderer {
+    cardContainer = document.getElementById('card-container');
+    addBookModal = document.getElementById("addBookModal");
+    addBookBtn = document.getElementsByClassName("add-book-btn")[0];
+    closeModalBtn = document.getElementById("closeModalBtn");
+    addBookForm = document.getElementById("form-add-book");
+    constructor (library) {
+        this.library = new Library();
+        this.configureModalEventListeners();
+    }
+    createCardElementFromBook(book){
+        let title = book.title; 
+        let author = book.author;
+        let numPages = book.numPages;
+        let haveRead = book.haveRead;
+        let id = book.id
+        let checkedStr = haveRead ? ' checked' : '';
+        const cardHTML = `
+            <div class="card" data-id=${id}>
+                <span class="delete-card-btn" data-id=${id}>&times;</span>
+                <div class="card-inner">
+                    <div class="card-upper">
+                        <p class="card-book-title">${title}</p>
+                        <p class="card-book-author">by ${author}</p>
+                        <p class="card-book-pages">${numPages} pages</p>
+                    </div>
+                    <div class="card-lower">
+                        <p>read?</p>
+                        <input class="card-book-hasRead" type="checkbox"${checkedStr} data-id=${id}>
+                    </div>
                 </div>
             </div>
-        </div>
-    `;
-    const tempDiv = document.createElement('div');
-    tempDiv.innerHTML = cardHTML;
-    const newCard = tempDiv.firstElementChild;
-    // console.log(newCard);
-    return newCard;
-}
-function addBookToPage(book){
-    let newCard = createCardElementFromBook(book);
-    let closeBtn = newCard.getElementsByClassName('delete-card-btn')[0];
-    closeBtn.addEventListener('click', (event) => {
-        deleteBook(closeBtn.getAttribute('data-id'));
-    });
-    let checkbox = newCard.getElementsByClassName('card-book-hasRead')[0];
-    checkbox.addEventListener('click', (event) => {
-        toggleHaveRead(newCard.id);
-    });
-    cardContainer.appendChild(newCard);
-}
-function addLibraryToPage(){
-    for (let book of myLibrary) {
-        addBookToPage(book);
+        `;
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = cardHTML;
+        const newCard = tempDiv.firstElementChild;
+        // console.log(newCard);
+        return newCard;
     }
-}
-function openAddBookModal(){
-    addBookModal.style.display = 'block';
-}
-function closeAddBookModal(){
-    addBookModal.style.display = 'none';
-}
-function handleFormSubmission() {
-    const formData = new FormData(addBookForm);
-    const title = formData.get('title');
-    const author = formData.get('author');
-    const numPages = formData.get('numPages');
-    let haveRead = (formData.get('haveRead') === "true") ? true : false;
-    closeAddBookModal();
-    addBookForm.reset();
-    const newBook = new Book(title, author, numPages, haveRead);
-    addBookToLibrary(newBook);
-    addBookToPage(newBook);
-
-}
-function configureModalEventListeners() {
-    addBookBtn.addEventListener('click', () => {
-        openAddBookModal();
-    });
-    closeModalBtn.addEventListener('click', () => {
-        closeAddBookModal();
-    });
-    window.addEventListener('click', (event) => {
-        if (event.target === addBookModal) {
-            closeAddBookModal();
-        }
-    });
-    addBookForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        handleFormSubmission();
-    });
-}
-function removeBookCardFromPage(id){
-    for (let child of cardContainer.children) {
-        if (id == child.getAttribute('data-id')) child.remove();
+    addBookToPage(book){
+        let newCard = this.createCardElementFromBook(book);
+        let closeBtn = newCard.getElementsByClassName('delete-card-btn')[0];
+        closeBtn.addEventListener('click', (event) => {
+            this.deleteBook(closeBtn.dataset.id);
+        });
+        let checkbox = newCard.getElementsByClassName('card-book-hasRead')[0];
+        checkbox.addEventListener('click', (event) => {
+            this.library.toggleHaveRead(newCard.dataset.id);
+        });
+        this.cardContainer.appendChild(newCard);
     }
-}
-function removeBookFromLibrary(id){
-    for (let i = 0; i < myLibrary.length; i++){
-        if (myLibrary[i].id === id) {
-            myLibrary.splice(i, 1);
-            i--;
+    addLibraryToPage(){
+        for (let book of this.library.bookArray) {
+            this.addBookToPage(book);
         }
     }
-}
-function deleteBook(id){
-    removeBookCardFromPage(id);
-    removeBookFromLibrary(id);
-}
-function toggleHaveRead(id) {
-    for (let i = 0; i < myLibrary.length; i++){
-        if (myLibrary[i].id == id) {
-            myLibrary[i].haveRead = !myLibrary[i].haveRead;
+    removeBookCardFromPage(id){
+        for (let child of this.cardContainer.children) {
+            if (id == child.getAttribute('data-id')) child.remove();
         }
     }
+    deleteBook(id){
+        this.removeBookCardFromPage(id);
+        this.library.removeBookFromLibrary(id);
+    }
+    openAddBookModal(){
+        this.addBookModal.style.display = 'block';
+    }
+    closeAddBookModal(){
+        this.addBookModal.style.display = 'none';
+    }    
+    handleFormSubmission() {
+        const formData = new FormData(this.addBookForm);
+        const title = formData.get('title');
+        const author = formData.get('author');
+        const numPages = formData.get('numPages');
+        let haveRead = (formData.get('haveRead') === "true") ? true : false;
+        this.closeAddBookModal();
+        this.addBookForm.reset();
+        const newBook = new Book(title, author, numPages, haveRead);
+        this.library.addBookToLibrary(newBook);
+        this.addBookToPage(newBook);
+    }
+    configureModalEventListeners() {
+        this.addBookBtn.addEventListener('click', () => {
+            this.openAddBookModal();
+        });
+        this.closeModalBtn.addEventListener('click', () => {
+            this.closeAddBookModal();
+        });
+        window.addEventListener('click', (event) => {
+            if (event.target === this.addBookModal) {
+                this.closeAddBookModal();
+            }
+        });
+        this.addBookForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            this.handleFormSubmission();
+        });
+    }
 }
-configureModalEventListeners();
 
 let bookTitle= 'myBook';
 let bookAuthor= 'myAuthor';
 let bookPages = 200;
 let bookHaveRead = false;
-let book1 = new Book(bookTitle, bookAuthor, bookPages, 
-    bookHaveRead);
-// console.log(book1);
+let book1 = new Book(bookTitle, bookAuthor, bookPages, bookHaveRead);
 let book2 = new Book('book2', 'that guy', 300, false);
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(new Book('title', 'author', 123, false));
-// addBookToLibrary(book2);
-// addBookToLibrary(book2);
-// addBookToLibrary(book2);
-// addBookToLibrary(book2);
-// // addBookToLibrary(book2);
-// addBookToLibrary(book2);
-addLibraryToPage();
+
+let libraryRender = new LibraryScreenRenderer();
+libraryRender.library.addBookToLibrary(book1);
+libraryRender.library.addBookToLibrary(book2);
+libraryRender.library.addBookToLibrary(new Book('title', 'author', 123, false));
+libraryRender.addLibraryToPage();
